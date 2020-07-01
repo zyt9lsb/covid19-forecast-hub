7531e78edc5c17044bcea60a5957acc456c4756d
Serena-Wang <wang225y@mtholyoke.edu>
2020-07-01 23:09:47 +0800

    clean up R folder

--- code/shiny/R/get_g_ensemble_quantiles.R
+++
@@ -1,9 +0,0 @@
-get_g_ensemble_quantiles<- function(d){
-  ggplot(d %>%
-           mutate(team_model = paste(team,model,sep="-")),
-         aes(x = quantile, y = team_model, fill = yes)) +
-    geom_tile() +
-    theme_bw() +
-    theme(legend.position = "none",
-          axis.text.x = element_text(angle = 90, hjust = 1))
-}
