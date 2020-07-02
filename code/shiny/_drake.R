d92b721eda525ce006d28da0e43795aa5b0f6115
Jarad Niemi <niemi@iastate.edu>
2020-07-02 15:42:20 -0500

    Move preamble from drake_plan.R file to _drake.R
    as it seems the forecast file locations are incorrect,
    this should ensure the forecast_files get read in immediately
    before making the plan

--- code/shiny/_drake.R
+++ code/shiny/_drake.R
@@ -1,4 +1,12 @@
 library("drake")
+
+R.utils::sourceDirectory("code/shiny/R",modifiedOnly=FALSE)
+
+forecast_files        = get_forecast_files()
+latest_forecast_files = get_latest_forecast_files(forecast_files)
+
+ids = rlang::syms(lapply(latest_forecast_files, function(f) unlist(strsplit(as.character(f), "/"))[2]))
+
 source("code/shiny/drake_plan.R")
 drake_config(plan, targets = shiny)
 #drake_config(plan, targets = all_forecasts)
--- code/shiny/drake_plan.R
+++ code/shiny/drake_plan.R
@@ -1,10 +1,3 @@
-R.utils::sourceDirectory("code/shiny/R",modifiedOnly=FALSE)
-
-forecast_files        = get_forecast_files()
-latest_forecast_files = get_latest_forecast_files(forecast_files)
-
-ids = rlang::syms(lapply(latest_forecast_files, function(f) unlist(strsplit(as.character(f), "/"))[2]))
-
 plan = drake::drake_plan(
   locations = get_locations(file_in("data-locations/locations.csv")),
   
